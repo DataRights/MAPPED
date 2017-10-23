2a75e193b2624be40dc3a9eff33a6b3387673128
ShaB <shab@cloud66.com>
2017-10-23 22:40:17 +0100

    Role validation

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test 'should have name' do
+		role = Role.new
+		assert_not role.valid?
+	end
 end
