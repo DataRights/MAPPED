99358ac25591045b6430b3009aacf4c464490282
shahriarb <shahriar.b@gmail.com>
2017-11-02 15:54:05 +0000

    liquid gem

--- Gemfile
+++ Gemfile
@@ -89,3 +89,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # End Default rails gems #
+
+gem 'liquid'
--- test/models/template_version_test.rb
+++ test/models/template_version_test.rb
@@ -3,5 +3,13 @@ require 'test_helper'
 class TemplateVersionTest < ActiveSupport::TestCase
   test "render" do
     assert_equal '', TemplateVersion.new.render(nil)
+
+    exception = assert_raise (RuntimeError ) {TemplateVersion.new(content: 'This is a template').render(nil)}
+    assert_equal 'Invalid context', exception.message
+
+    exception = assert_raise (RuntimeError ) {TemplateVersion.new(content: 'This is a template').render('wrong context')}
+    assert_equal 'Wrong context type', exception.message
+
+    assert_equal 'This is a template', TemplateVersion.new(content: 'This is a template').render(TemplateContext.new)
   end
 end
