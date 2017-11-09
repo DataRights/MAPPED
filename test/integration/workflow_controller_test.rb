0e0c6954236992a1515ecc50ea0c6f91b1b369de
mani47 <mani@acceptto.com>
2017-11-09 22:29:07 +0400

    layout UX and home page view added

--- test/integration/workflow_controller_test.rb
+++ test/integration/workflow_controller_test.rb
@@ -4,6 +4,6 @@ class WorkflowControllerTest < ActionDispatch::IntegrationTest
   test "calling diagram with invalid params should return error messages" do
     get "/workflow/diagram/s"
     assert_response :success
-    assert_text I18n.t('invalid_parameters')
+    assert @response.body.include?(I18n.t('invalid_parameters'))
   end
 end
