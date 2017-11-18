5090f2bc6f3eba653233ceb10c2dce2514fe5261
mani47 <mani@acceptto.com>
2017-11-19 00:54:48 +0400

    change default email address for sendgrid

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -72,6 +72,11 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_options = {
+      :from => 'mapped@herokumanager.com',
+      :reply_to => 'mapped@herokumanager.com'
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mapped_#{Rails.env}"
