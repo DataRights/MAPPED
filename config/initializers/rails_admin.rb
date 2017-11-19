f5e8907a1c27e64e038ef7f12c316e7f769ac320
mani47 <mani@acceptto.com>
2017-11-19 22:50:03 +0400

    add parent-child relation to admin menus for grouping

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -53,6 +53,7 @@ RailsAdmin.config do |config|
   end
 
   config.model TemplateVersion do
+    parent Template
     edit do
       field :version
       field :template
@@ -61,4 +62,87 @@ RailsAdmin.config do |config|
       field :language
     end
   end
+
+  config.model WorkflowTypeVersion do
+    list do
+      field :version
+      field :workflow_type
+      field :active
+
+      field :diagram do
+        pretty_value do
+          %{<a id="workflow_diagram_link" href="/workflow/diagram/#{value}" target='_blank'>#{I18n.t('workflow.generate_diagram')}</a>}.html_safe
+        end
+      end
+    end
+  end
+
+  config.model Answer do
+    parent Question
+  end
+
+  config.model UserRole do
+    parent User
+  end
+
+  config.model AccessRight do
+    parent User
+  end
+
+  config.model Notification do
+    parent User
+  end
+
+  config.model Address do
+    parent User
+  end
+
+  config.model Role do
+    parent User
+  end
+
+  config.model City do
+    parent User
+  end
+
+  config.model Country do
+    parent User
+  end
+
+  config.model WorkflowState do
+    parent Workflow
+  end
+
+  config.model WorkflowTransition do
+    parent Workflow
+  end
+
+  config.model WorkflowType do
+    parent Workflow
+  end
+
+  config.model WorkflowTypeVersion do
+    parent Workflow
+  end
+
+  config.model WorkflowState do
+    parent Workflow
+  end
+
+  config.model Transition do
+    parent Workflow
+  end
+
+  config.model Guard do
+    parent Workflow
+  end
+
+  config.model CodeAction do
+    parent Workflow
+  end
+
+  config.model Organization do
+    parent Sector
+  end
+
 end
