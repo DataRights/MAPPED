809dd7cc094d1b9560c176ae64f9c983d468598a
shahriarb <shahriar.b@gmail.com>
2017-11-22 22:53:13 +0000

    500K limit for attachment size

--- test/models/attachment_test.rb
+++ test/models/attachment_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AttachmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "max size of attachment is 500k" do
+    wt = workflow_transitions(:one)
+    attachment = Attachment.new(workflow_transition: wt)
+		assert attachment.valid?
+    attachment.content = '0' * Attachment::MAX_SIZE
+    assert attachment.valid?
+    attachment.content += '0'
+    assert_not attachment.valid?
+    assert_includes  attachment.errors[:content], I18n.t('validations.attachment_max_size')
+  end
 end
