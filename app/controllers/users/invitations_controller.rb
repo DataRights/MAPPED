76966587334fde29676fec94aff40f8a5362966a
shahriarb <shahriar.b@gmail.com>
2017-11-27 20:38:01 +0000

    use $ in custom invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -4,7 +4,9 @@ class Users::InvitationsController < Devise::InvitationsController
   def create
     if params[:customized_invitation]
       user = User.invite!(:email => params[:user][:email], :skip_invitation => true)
-      UserMailer.custom_invitation(user,params[:customized_invitation_content]).deliver
+      user.send(:generate_invitation_token!)
+      mail_content = params[:customized_invitation_content].gsub('$ACCEPT_INVITATION_URL$',Rails.application.routes.url_helpers.accept_user_invitation_url(:host => Rails.configuration.base_url, :invitation_token => user.raw_invitation_token))
+      UserMailer.custom_invitation(user,mail_content).deliver
       user.invitation_sent_at = DateTime.now
       user.save!
       redirect_to root_path
