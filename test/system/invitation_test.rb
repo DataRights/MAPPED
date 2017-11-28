779e47374026f66b0eeaedd9b63767779cef7aad
shahriarb <shahriar.b@gmail.com>
2017-11-28 14:49:08 +0000

    system tests for invitations

---
+++ test/system/invitation_test.rb
@@ -0,0 +1,93 @@
+require 'application_system_test_case'
+
+class InvitationTest < ApplicationSystemTestCase
+  setup do
+    @sample_email = 'john@smith.com'
+    @sample_password = '1234567890'
+    User.create!(email: @sample_email, password_confirmation: @sample_password, password: @sample_password, roles: [Role.find_by(name: roles(:admin).name)])
+    @user = User.find_by(email: @sample_email)
+    @user.confirm
+  end
+
+  teardown do
+    User.find_by(email: @sample_email).destroy
+  end
+
+  test 'Send standard invite' do
+    # 1. Login
+    assert_nil @user.encrypted_otp_secret # OTP is not enabled for user
+    visit('/admin')
+    fill_in('user_email', with: @sample_email)
+    fill_in('user_password', with: @sample_password)
+    click_button I18n.t('devise.sign_in', default: 'Sign in')
+    assert page.has_content?(I18n.t('admin.actions.dashboard.title').upcase)
+
+    user = User.find_by(email: 'test_invite@test.com')
+    assert_not user
+
+    visit(new_user_invitation_path)
+    fill_in('user_email', with: 'test_invite@test.com')
+    click_button I18n.t("devise.invitations.new.submit_button")
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal 'Invitation instructions', mail.subject
+    user = User.find_by(email: 'test_invite@test.com')
+    assert user
+    assert user.created_by_invite?
+    user.destroy
+  end
+
+  test 'Send custom invite' do
+    # Login
+    assert_nil @user.encrypted_otp_secret # OTP is not enabled for user
+    visit('/admin')
+    fill_in('user_email', with: @sample_email)
+    fill_in('user_password', with: @sample_password)
+    click_button I18n.t('devise.sign_in', default: 'Sign in')
+    assert page.has_content?(I18n.t('admin.actions.dashboard.title').upcase)
+
+    user = User.find_by(email: 'test_invite@test.com')
+    assert_not user
+
+    visit(new_user_invitation_path)
+    fill_in('user_email', with: 'test_invite@test.com')
+    fill_in('customized_invitation_content', with: 'this is an invitation')
+    click_button I18n.t("devise.invitations.new.submit_custom")
+    assert_not ActionMailer::Base.deliveries.empty?
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal I18n.t('devise.invitations.new.custom_email_subject'), mail.subject
+    assert mail.body.parts.first.body.raw_source.include?('this is an invitation')
+    user = User.find_by(email: 'test_invite@test.com')
+    assert user
+    assert user.created_by_invite?
+    user.destroy
+  end
+
+  test 'Send custom invite with confirmation url' do
+    # Login
+    assert_nil @user.encrypted_otp_secret # OTP is not enabled for user
+    visit('/admin')
+    fill_in('user_email', with: @sample_email)
+    fill_in('user_password', with: @sample_password)
+    click_button I18n.t('devise.sign_in', default: 'Sign in')
+    assert page.has_content?(I18n.t('admin.actions.dashboard.title').upcase)
+
+    user = User.find_by(email: 'test_invite@test.com')
+    assert_not user
+
+    visit(new_user_invitation_path)
+    fill_in('user_email', with: 'test_invite@test.com')
+    fill_in('customized_invitation_content', with: 'click $ACCEPT_INVITATION_URL$ yes')
+    click_button I18n.t("devise.invitations.new.submit_custom")
+    assert_not ActionMailer::Base.deliveries.empty?
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal I18n.t('devise.invitations.new.custom_email_subject'), mail.subject
+    assert mail.body.parts.first.body.raw_source.include?('accept?invitation_token=')
+    user = User.find_by(email: 'test_invite@test.com')
+    assert user
+    assert user.created_by_invite?
+    user.destroy
+  end
+
+end
