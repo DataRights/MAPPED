25d31fdf10054f7a13b7571369c299f92d5ca46b
shahriarb <shahriar.b@gmail.com>
2017-12-03 21:25:18 +0000

    fix tests for authentication

--- test/controllers/access_requests_controller_test.rb
+++ test/controllers/access_requests_controller_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class AccessRequestsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:test_user)
+    @user.confirm
+    sign_in @user
+    Campaign.create(:name => 'Default') unless Campaign.find_by(:name => 'Default')
+  end
+
   test "should get index" do
     get access_requests_index_url
     assert_response :success
@@ -9,6 +18,8 @@ class AccessRequestsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get access_requests_new_url
     assert_response :success
+
+    
   end
 
   test "should get create" do
