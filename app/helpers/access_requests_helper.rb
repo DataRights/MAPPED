4166435ddf56fa9d536cb535a271251351ccf218
shahriarb <shahriar.b@gmail.com>
2017-12-06 23:15:16 +0000

    AccessRequestHelper get_template

--- app/helpers/access_requests_helper.rb
+++ app/helpers/access_requests_helper.rb
@@ -15,4 +15,29 @@ module AccessRequestsHelper
     campaign.organizations.where(sector: sector).map {|organization| [organization.name, organization.id]}
   end
 
+  def get_template(user, campaign, organization)
+    return nil unless user
+    return nil unless user.is_a? User
+    return nil unless campaign
+    return nil unless campaign.is_a? Campaign
+    return nil unless organization
+    return nil unless organization.is_a? Organization
+    sector = organization.sector
+    return nil unless sector
+
+    expected_langs = organization.languages
+    accepted_versions = []
+    active_templates = sector.templates.joins(:template_versions).where(:template_versions => {:active => true})
+    active_templates.each do |template|
+      template.template_versions.where(:active => true).each do |active_version|
+        accepted_versions << active_version if expected_langs.include? active_version.language.to_sym
+      end
+    end
+    return nil if active_versions.blank?
+    prefered_lang = user.preferred_language
+    prefered_lang ||= :en
+    result = accepted_versions.detect {|active_version| active_version.language == prefered_lang}
+    result ||= accepted_versions.first
+    result
+  end
 end
