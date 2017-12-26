d5c102a581cd9af7b5a7d1d4ba08a92e3f0ae87c
Shahriar Boroujerdin <shahriar.boroujerdin@shahriarboroujerdin.local>
2017-12-26 22:22:40 +0000

    Evaluate form basic

--- app/helpers/access_requests_helper.rb
+++ app/helpers/access_requests_helper.rb
@@ -14,4 +14,41 @@ module AccessRequestsHelper
     return Organization.where(sector: sector).map {|organization| [organization.name, organization.id]} if campaign.name == Campaign::DEFAULT_CAMPAIGN_NAME
     campaign.organizations.where(sector: sector).map {|organization| [organization.name, organization.id]}
   end
+
+  def generate_question_view(question)
+    result = ''
+    return result unless question
+    return result unless question.is_a? Question
+    result = question.title + ' <br>'
+    case question.type
+    when 'QuestionSimple'
+      result += generate_simple_question(question)
+    when 'QuestionMultiple'
+      result += generate_multiple_question(question)
+    when 'QuestionSelectList'
+      result += generate_select_question(question)
+    end
+    result
+  end
+
+  protected
+
+  def generate_simple_question(question)
+    return '' unless question.is_a? QuestionSimple
+    text_field_tag(attribute_id(question))
+  end
+
+  def generate_multiple_question(question)
+    return '' unless question.is_a? QuestionMultiple
+    select_tag(attribute_id(question),options_for_select(question.metadata['option_list']))
+  end
+
+  def generate_select_question(question)
+    return '' unless question.is_a? QuestionSelectList
+    select_tag(attribute_id(question),options_for_select(question.metadata['option_list']))
+  end
+
+  def attribute_id(question)
+    "answers[#{question.id}]"
+  end
 end
