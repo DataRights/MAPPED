a67d2c919b6642333df1b6d63c17630d1cf2482a
mani47 <mani@acceptto.net>
2018-01-26 11:45:53 +0400

    make flash messages dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_class(key)
+    case key
+    when "notice" then "alert alert-success alert-dismissible"
+    when "alert" then "alert alert-danger alert-dismissible"
+    when "success" then "alert alert-success alert-dismissible"
+    end
+  end
 end
