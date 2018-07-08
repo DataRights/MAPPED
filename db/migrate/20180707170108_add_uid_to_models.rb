a25a4835acea4a949f28b06a5bb594289660db8f
Mani Malekmohammadi <mm.mani@gmail.com>
2018-07-08 13:50:52 -0700

    remove enable extension

--- db/migrate/20180707170108_add_uid_to_models.rb
+++ db/migrate/20180707170108_add_uid_to_models.rb
@@ -1,6 +1,6 @@
 class AddUidToModels < ActiveRecord::Migration[5.1]
   def change
-    enable_extension 'pgcrypto'
+    #enable_extension 'pgcrypto'
     add_column :access_requests, :uid, :uuid, default: 'gen_random_uuid()', null: false
     add_column :attachments, :uid, :uuid, default: 'gen_random_uuid()', null: false
   end
